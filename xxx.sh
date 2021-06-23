5409536989871a4b168339aa95c8972aa2b50557
BakayDev <gamempvs@gmail.com>
2021-06-23 22:38:04 +0600

    DOCKER apache: added Dockerfile and update command file

--- xxx.sh
+++ xxx.sh
@@ -1,1 +1,9 @@
 docker run --rm php:7.4-cli-alpine php -v
+
+
+docker build --tag=site-apache site
+
+docker run -d --rm --name app-v1 -p 8088:80 site-apache
+
+docker run -d --name app-v1 -p 8088:80 site-apache
+docker stop app-v1 && docker rm app-v1
